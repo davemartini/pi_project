9eb15868bea86630774c0b167ed934ae6584965d
davemartini <dmarti10@rams.shepherd.edu>
2021-04-17 21:28:41 -0400

    Lotta testing, blink demo

--- Hello.py
+++ Hello.py
@@ -1,3 +1,38 @@
-import RPi.GPIO
+import RPi.GPIO as GPIO
+import time, sched, _thread
 
-print("hello world")
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.OUT)
+
+GPIO.output(18, False)
+
+
+
+
+
+
+
+def blinker():
+    GPIO.output(18, True)
+    time.sleep(2)
+    
+def offer():
+    GPIO.output(18, False)
+    time.sleep(2)
+
+for x in range(5):
+    blinker()
+    offer()
+    
+GPIO.cleanup()
+
+    
+    
+    
+    
+
+
+
+#print("hello world")
+
+
