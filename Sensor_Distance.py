87eb690a5446d8c9bd7c739141f06eea80d67564
davemartini <dmarti10@rams.shepherd.edu>
2021-04-26 22:25:21 -0400

    more comments, everything should be explained now

--- Sensor_Distance.py
+++ Sensor_Distance.py
@@ -19,13 +19,16 @@ GPIO.output(BUZZER, False)
 
 
 def armed():
-    GPIO.output(LIGHT, True)
+    GPIO.output(LIGHT, True) #Turns on LED indicating the system is armed and polling
 
 
-def distanceMath(start, end): #distance math function 
+def distanceMath(start, end):
+    #distance math function. distance = speed * Time, however input from the echo pin is time from the emitter
+    #to the object and back to the reciever, so the time must be /2 to get only the distance to the object. Thus the distance is time-duration * (34300/2)
     duration = end-start
     distance = (duration * 17150)
     distance = round(distance, 2)
+    #speed of sound is 343m/s which is = to 34300cm/s. 
 
     return distance
 
@@ -37,7 +40,8 @@ def pollPosition():
         ##time.sleep(.00001)
         GPIO.output(TRIG,False)
 
-        while GPIO.input(ECHO) == 0: #echo is the input pin on the sensor, after pulse is sent, the sensor reads a zero and sets time to start then when input is read it is set to time end
+        while GPIO.input(ECHO) == 0: #echo is the input pin on the sensor, after pulse is sent, the sensor reads the time when it was last a zero or low signal and sets time to start
+            #then when input is read, or rather echo reads a high signal or a 1 it is set to time end
             start = time.time()
 
         while GPIO.input(ECHO) == 1:
@@ -64,25 +68,25 @@ def setThresh(): #runs same function as pollPosition but sets a global variable 
     thresh = distanceMath(start,end) #alarm threshold calculated in cm
 
 def checkThresh(currentDistance):
-    threshDeviation = thresh+5 #adds five cm to the threshold measurement to decrease the likelihood of a false positive alarm
+    threshDeviation = thresh+5 #adds five cm to the threshold measurement to decrease the likelihood of a false positive alarm and control for calibration errors
 
-    if(currentDistance > threshDeviation):
+    if(currentDistance > threshDeviation): #if polled distance is greater than the threshold it triggers the alarm
         alarm()
 
-def alarm(): #alarm function triggers the pin to send up signal to buzzer, setting it off and leaving it on until manually interrupted by keyboard
+def alarm(): #alarm function triggers the pin to send up signal to buzzer, setting it off and leaving it on until manually interrupted by keyboard, essentially end condition
     while True:
         GPIO.output(BUZZER, True)
 
 try:
     while True: #main while loop that loops awaiting input from the button to bring the signal to the down position to trigger the alarm process
-        untriggered = GPIO.input(23) #untriggered initilizes to 1 as was set at the begining of the script setup
-        if(untriggered == 0):
+        untriggered = GPIO.input(BUTTON) #untriggered initilizes to 1 as was set at the begining of the script setup
+        if(untriggered == 0): #when button is pressed the system calls functions to arm the system at the current position and poll until interrupted by keyboard
             armed()
             setThresh()
             pollPosition()
 
 
 finally:
-    GPIO.cleanup()
+    GPIO.cleanup() #when interrupted by keyboard, finally block executes and cleans up the pins so there is no hold over in-use pin conflicts between runs
 
 
